f4df7301e9aef9f38ff1f876c49f3365c150748a
Kubi <KubiHUN@users.noreply.github.com>
2021-10-20 12:04:41 +0200

    Add countries

---
+++ Countries/countries.js
@@ -0,0 +1,11 @@
+function loadCountry() {
+    fetch("https://restcountries.com/v3.1/name/peru").then(r => r.json()).then(showCountry);
+}
+
+function showCountry(country) {
+    console.log(country);
+    const NAME = country[0].name.official;
+    document.getElementById("name").textContent = NAME;
+}
+
+window.addEventListener("load", loadCountry);
